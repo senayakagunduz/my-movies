d328e5246af012f341bd7034fbb95706ca2746e4
senayakagunduz <senayakgndz@gmail.com>
2022-03-15 01:57:02 +0300

    axios and fake api

--- src/components/App.js
+++ src/components/App.js
@@ -1,101 +1,43 @@
+import axios from "axios";
 import React from "react";
 import MovieList from "./MovieList";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   state = {
-    movies: [
-      {
-        id: 1,
-        name: "The Matrix 3",
-        rating: "8.1",
-        overview:
-          "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.",
-        imageURL:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/dXNAPwY7VrqMAo51EKhhCJfaGb5.jpg",
-      },
-      {
-        id: 2,
-        name: "Superman",
-        rating: 7.6,
-        overview:
-          "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        imageURL:
-          "https://image.tmdb.org/t/p/w220_and_h330_face/6Bbq8qQWpoApLZYWFFAuZ1r2gFw.jpg",
-      },
-      {
-        id: 3,
-        name: "The Matrix Reloaded",
-        rating: "6.9",
-        imageURL:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/jBegA6V243J6HUnpcOILsRvBnGb.jpg",
-        overview:
-          "Six months after the events depicted in The Matrix, Neo has proved to be a good omen for the free humans, as more and more humans are being freed from the matrix and brought to Zion, the one and only stronghold of the Resistance. Neo himself has discovered his superpowers including super speed, ability to see the codes of the things inside the matrix and a certain degree of pre-cognition.",
-      },
-      {
-        id: 4,
-        name: "Saw 3D",
-        rating: "7.5",
-        overview:
-          "SAW legacy, a group of Jigsaw survivors gathers to seek the support of self-help guru and fellow survivor Bobby Dagen, a man whose own dark secrets unleash a new wave of terror.",
-        imageURL:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/qHCZ6LjtmqWDfXXN28TlIC9OppK.jpg",
-      },
-      {
-        id: 5,
-        name: "Blitz 007",
-        rating: "11",
-        overview:
-          "A tough, renegade cop with a gay sidekick is dispatched to take down a serial killer who has been targeting police officers. AÇIKLAMA AÇIKLAMA",
-        imageURL:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/qCPMjT8Ld8tvs1zs7LY2jpKlRIK.jpg",
-      },
-      {
-        id: 6,
-        name: "Hostage",
-        rating: "6.3",
-        imageURL:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/4hne3v6jN4MlCnhSkxOW7YspJhr.jpg",
-        overview:
-          "When a mafia accountant is taken hostage on his beat, a police officer – wracked by guilt from a prior stint as a negotiator – must negotiate the standoff, even as his own family is held captive by the mob.",
-      },
-      {
-        id: 7,
-        name: "The Matrix Reloaded",
-        rating: "6.9",
-        imageURL:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/jBegA6V243J6HUnpcOILsRvBnGb.jpg",
-        overview:
-          "Six months after the events depicted in The Matrix, Neo has proved to be a good omen for the free humans, as more and more humans are being freed from the matrix and brought to Zion, the one and only stronghold of the Resistance. Neo himself has discovered his superpowers including super speed, ability to see the codes of the things inside the matrix and a certain degree of pre-cognition.",
-      },
-      {
-        id: 8,
-        name: "Rogue",
-        rating: 7.4,
-        overview:
-          "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        imageURL:
-          "https://image.tmdb.org/t/p/w220_and_h330_face/uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
-      },
-
-      {
-        id: 9,
-        name: "Project Power",
-        rating: 6.7,
-        overview:
-          "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        imageURL:
-          "https://image.tmdb.org/t/p/w220_and_h330_face/TnOeov4w0sTtV2gqICqIxVi74V.jpg",
-      },
-    ],
+    movies: [],
     searchQuery: "",
   };
-  deleteMovie = (movie) => {
+  /* async componentDidMount() {
+    const baseURL = "http://localhost:3008/movies";
+    const response = await fetch(baseURL);
+    const data = await response.json();
+    console.log(data);
+    this.setState({ movies: data });
+  } */ /*npm install axios yapıp axios kütüphanesini yükleyip veritabanına erişicez*/
+  async componentDidMount() {
+    const response = await axios.get("http://localhost:3008/movies");
+    this.setState({ movies: response.data });
+  }
+
+  /* deleteMovie = (movie) => { //normal silme
     const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
     this.setState((state) => ({
       movies: newMovieList,
     }));
+  }; */
+  /*deleteMovie = async (movie) => { //Fetch ile olan
+    const baseURL = `http://localhost:3008/movies/${movie.id}`;
+    await fetch(baseURL, { method: "Delete" });
+  };*/
+  /*axios api */
+  deleteMovie = async (movie) => {
+    //async await demek adresten veriyi almadan veriyi getirmeden çalışmayacaksın anlamına geliyor.
+    axios.delete(`http://localhost:3008/movies/${movie.id}`);
+    const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+    this.setState((state) => ({ movies: newMovieList }));
   };
+
   searchMovie = (event) => {
     //console.log(event.target.value);
     this.setState({ searchQuery: event.target.value });
